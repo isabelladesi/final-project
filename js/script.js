0412ace262fc4be9910ef5e32f1c02ded3abfd9e
isabelladesi <114624641+isabelladesi@users.noreply.github.com>
2024-04-23 11:02:32 -0400

    Delete js/script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-100px"; // Hide navbar
-//   }
-// }
